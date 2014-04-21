c249ef5c58c92333f7a046d0e2cb5291d8a9f487
Raphael <zyorn.mail@gmail.com>
2014-04-21 16:20:38 +0100

    6.3.2 Password and Confirmation with has_secure_password

--- Gemfile
+++ Gemfile
@@ -2,10 +2,11 @@ source 'https://rubygems.org'
 ruby '2.0.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.0.0'
+gem 'rails', '4.0.4'
 gem 'bootstrap-sass', '2.3.2.0'
 gem 'sprockets', '2.11.0'
-gem 'bcrypt-ruby', '3.1.5'
+gem 'bcrypt-ruby', '3.1.2'
+
 #gem 'rubysl-securerandom', '2.0.0'
 gem 'bundler', '1.5.2'
 gem 'mspec', '1.5.21'
@@ -16,7 +17,7 @@ gem 'rake', '10.3.1'
 group :development, :test do
 #gem 'pg', '0.15.1'
   gem 'sqlite3', '1.3.8'
-  gem 'rspec-rails', '2.13.1'
+  gem 'rspec-rails', '2.14.2'
 end
 
 group :test do
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140421135647) do
+ActiveRecord::Schema.define(version: 20140421141546) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20140421135647) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "password"
+    t.string   "password_confirmation"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
